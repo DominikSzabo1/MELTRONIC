6b5c15723ffd999f2837b2b19ad038e6920f4098
Szabo, Dominik <Dominik.Szabo@mdc-berlin.de>
2024-01-20 23:36:16 +0100

    inital commit

---
+++ long_matrix_to_IS.R
@@ -0,0 +1,168 @@
+suppressMessages(require(data.table))
+suppressMessages(require(stringr))
+suppressMessages(require(argparser))
+
+#parse arguments:
+parser <- arg_parser('Calculate insulation scores of a 3-column long matrix in a_start, b_start, value format at user specified insulation square sizes. Chromosome must be encoded in the filename.', hide.opts = TRUE)
+
+parser <- add_argument(parser, "--cores", type="integer", default=NULL,
+                       help="Indicate how many cores should be used for computation. If not set, data.table reads environment variables and uses all ligcal CPUs available")
+parser <- add_argument(parser, "--ISmin", short='--ISmin', type="integer", default=100000,  
+                       help="Which is the smallest insulation square size in bp for which IS should be calculated")
+parser <- add_argument(parser, "--ISmax", short='--ISmax', type="integer", default=1000000, 
+                       help="Which is the largest insulation square size in bp for which IS should be calculated")
+parser <- add_argument(parser,"--ISsteps", short='--ISsteps', type="integer", default=10, 
+                       help="How many different resolutions between ISmin and ISmax should be calculated")
+parser <- add_argument(parser, "--input", nargs=Inf,
+                       help='Input at least one long matrix in c(\"A_start\", \"B_start\", \"value\") format or use wildcard \"*\" to process multiple chromosomes')
+parser <- add_argument(parser, "--outfile", default=paste0(getwd(), '/IS_values.tsv'),
+                       help="Indicate path and filename to which output table should be saved. Add '.gz' at end of filename to compress")
+parser <- add_argument(parser, "--resolution", default=NULL,
+                       help='Specify the resolution of the matrix in bp. E.g.: For 10kb resolution put --resolution 10000')
+
+
+
+main <- function() {
+  args <- parse_args(parser)
+  
+  #catch error: no matrix supplied
+  if(is.na(args$input[1])){
+    stop('NO input matrices supplied to function. Please specify at least one matrix in long format for calculation of IS ')
+  }
+  #catch error: no resolution
+  if(is.na(args$resolution)){
+    stop('Resolution of the matrix not supplied. Please specify the resolution after the --resolution flag')
+  }
+  
+  #Print out how many matrices will be converted
+  if(length(args$input)==1){
+    print('IS will be calculated for 1 matrix')
+  }
+  else{
+    print(paste('IS will be calculated for', length(args$input), 'matrices'))
+  }
+  
+  #set number of threads to be used by data.table:
+  setDTthreads(threads=parser$cores)
+  
+  #initialize a data frame to which mean contacts can be added
+  loop_out <- data.table(chrom = character(), viewpoint=integer(), mean_contacts=double(), IS_distance=integer())
+  
+  #catch error if no writing permission in outfile directory
+  if(file.create(args$outfile) == FALSE){
+    stop('No writing permission in the --outfile directory')
+  }
+  
+  #loop through all specified files
+  for (file in args$input) {
+    #Read in the matrix in long format
+    matrix_long <- fread(file)
+    
+    
+    #catch error if matrix has wrong format
+    if(dim(matrix_long)[2] != 3){
+      stop('matrix does not have 3 columns. Input a matrix in c(\"A_start\", \"B_start\", \"value\") format')
+    }
+    
+    #rename or set column names
+    setnames(matrix_long, c('A_start', 'B_start', 'count'))
+    
+    #determine first and last bins of that chromosome (how long is the chromosome)
+    matrix_minmax <- range(matrix_long$A_start)
+    
+    #drop the NAs from the table
+    matrix_long <- matrix_long[!is.na(matrix_long$count)]
+    
+    #determine matrix resolution from command line input
+    matrix_res <- as.integer(args$resolution)
+    matrix_resolution_kb <- paste0(matrix_res/1000, 'kb')
+
+    #add column which indicates how far appart bins A and B are
+    matrix_long$contact_distance = matrix_long$B_start - matrix_long$A_start
+    #remove contacts of directly neighboring bins and matrix diagonal
+    matrix_long_no_diagonal = matrix_long[contact_distance > matrix_res]
+
+    
+    #calculate required IS square sizes: defaults to 2 - 20 x matrix resolution in increments of 2 (=10 IS square sizes)
+    IS_size_min = args$ISmin
+    IS_size_max = args$ISmax
+    IS_size_min_to_max = seq(IS_size_min, 
+                             IS_size_max,
+                             length.out = args$ISsteps)
+    
+    
+    #catch error if matrix resolution does not allow IS calculation:
+    if(IS_size_min <= matrix_res){
+      stop('--ISmin is smaller or equal to the matrix resolution')
+    }
+    if(IS_size_max %% matrix_res != 0 |
+       IS_size_min %% matrix_res != 0){
+      stop('--ISmax or --ISmin is not a multiple of the matrix resolution')
+    }
+    
+    if(sum(IS_size_min_to_max %% matrix_res) != 0){
+      stop('Insulation square size specified through --ISsteps does not match the matrix resolution')
+    }
+    if(IS_size_max < IS_size_min){
+      stop('--ISmax is smaller than --ISmin')
+    }
+    
+    #determine which chromosome is read in from filename
+    splitted_filename <- str_split(file, "_")
+    chromosome_ID_withFilesuffix <- splitted_filename[[1]][str_detect(unlist(splitted_filename), '^chr')][1]
+    chromosome_ID_withFilesuffix_splitted <- str_split(chromosome_ID_withFilesuffix, "\\.")
+    chromosome_ID <- chromosome_ID_withFilesuffix_splitted[[1]][str_detect(unlist(chromosome_ID_withFilesuffix_splitted), '^chr')][1]
+    print(paste('Read in long format matrix of:', chromosome_ID, 'at', matrix_resolution_kb, 'kb resolution', 'from file:', file))
+    
+    #loop through all IS distances
+    for(IS_distance in IS_size_min_to_max){
+      #expected_number_of_values = IS_distance/matrix_res * IS_distance/matrix_res
+      print(paste('calculating mean contacts within IS square of size:', IS_distance))
+      
+      # remove everything that is further away than the IS square
+      matrix_long_no_diagonal_filtered <- matrix_long_no_diagonal[contact_distance <= IS_distance * 2] 
+      
+      #how many values are expected in the insulation square of size IS_distance? Make mean count of values in IS square NA if more than half of them are NA
+      expected_value_count <- (IS_distance/matrix_res)^2
+      
+      #loop through all unique start positions
+      for(viewpoint in unique(matrix_long_no_diagonal_filtered$A_start)){
+        matrix_sub <- matrix_long_no_diagonal_filtered[A_start >= (viewpoint - IS_distance) &
+                                                         B_start <= (viewpoint + IS_distance)  ]
+        matrix_sub$off_square = dplyr::case_when(matrix_sub$A_start < viewpoint & matrix_sub$B_start <= viewpoint ~TRUE,
+                                                 matrix_sub$A_start >= viewpoint & matrix_sub$B_start > viewpoint ~ TRUE,
+                                                 TRUE~FALSE)
+        matrix_sub <- matrix_sub[off_square == FALSE]
+        
+        #add row if more than half of the values in the insulation square are there
+        if(length(matrix_sub$count) >= expected_value_count/2 & 
+           viewpoint > matrix_minmax[1] + IS_distance & #do not add to list if viewpoint is too close to start or end of chromosome
+           viewpoint < matrix_minmax[2] - IS_distance){
+        loop_out <- rbind(loop_out, 
+                          list(chrom = chromosome_ID, 
+                               viewpoint = as.integer(viewpoint), 
+                               mean_contacts =  mean(matrix_sub$count), 
+                               IS_distance=as.integer(IS_distance)))
+        }
+      }
+    }
+  }
+  
+  #calculate mean number of contacts per IS square size, per chromosome for IS calculation:
+  mean_contacts_perchrom <- loop_out[ , keyby=.(chrom, IS_distance),
+                                       .(mean_perchrom=mean(mean_contacts, na.rm=TRUE))]
+  IS_table <- merge(loop_out, mean_contacts_perchrom, all.x=TRUE, by=c('chrom', 'IS_distance')) #left join to allow rowwise computation of the IS per chrom and IS_distance
+  IS_table[, 'IS' :=  .(log2(mean_contacts) - log2(mean_perchrom))]
+  IS_table[, c('mean_contacts', 'mean_perchrom') := NULL]
+  
+  #reorder columns
+  setcolorder(IS_table, c('chrom', 'viewpoint', 'IS_distance', 'IS'))
+  #order chromosomes
+  setattr(IS_table$chrom, 'levels', str_sort(unique(IS_table$chrom), numeric=TRUE))
+  #order by chrom, viewpoint and IS
+  IS_table <- IS_table[order(rank(chrom), viewpoint, IS_distance)]
+  
+  fwrite(x = IS_table, file=args$outfile, sep = '\t', na = 'NA', quote=FALSE, append = FALSE)
+}
+  
+main()
---
+++ matrix_wide_to_long.R
@@ -0,0 +1,174 @@
+#.libPaths( c( .libPaths(), "/home/dszabo/R/x86_64-redhat-linux-gnu-library/3.6/") )
+#suppressMessages(require(R.utils))
+suppressMessages(require(data.table))
+suppressMessages(require(stringr))
+suppressMessages(require(argparser))
+
+#print(.libPaths())
+
+
+parser <- arg_parser('Convert a square matrix into a long one', hide.opts = TRUE)
+
+parser <- add_argument(parser, "--cores", type="integer", default=NULL,
+                       help="Indicate how many cores should be used for computation. If not set, data.table reads environment variables and uses all ligcal CPUs available")
+parser <- add_argument(parser, "--input", nargs=Inf,
+                       help='Specify the path to at least one square matrix or use wildcard \"*\" to process multiple chromosomes')
+parser <- add_argument(parser, "--outfile", default=getwd(),
+                       help="For processing of multiple files: Indicate path to which output tables should be saved \n 
+                       If only a single file is processed, you can sepcify an path + filename")
+parser <- add_argument(parser, "--distanceCutoff",type="integer", 
+                       help='Specify a maximum distance of considered interactions in bp. Must be at least twice the value of the maximally considered IS distance! E.g. To remove contacts exceeding 2Mb of distance, specify -d 2000000')
+parser <- add_argument(parser, "--expectedContacts", default='cis',
+                       help='If the supplied matrix is genome wide (and contains interchromosomal contacts), please indicate "trans".')
+
+
+main <- function() {
+  args <- parse_args(parser)
+  filenames <- args$input
+  scope <- args$expectedContacts
+  
+  #catch error: scope is neither cis nor trans
+  if(sum(scope != c('cis', 'trans')) > 1){
+    stop('expectedContacts argument must be either "cis" or "trans"')
+  }  
+  
+  #catch error: no matrix supplied
+  if(is.na(filenames[1])){
+    stop('NO input matrices supplied to function. Please specify at least one matrix in long format for calculation of IS ')
+  }
+  
+  #Print out how many matrices will be converted
+  if(length(filenames)==1){
+    print('1 matrix will be converted to long format')
+  }
+  if(length(filenames) > 1){
+    print(paste(length(filenames), 'matrices will be converted to long format'))
+  }
+  
+  ###Convert cis matrices
+  if(scope == 'cis'){
+    distanceCutoff = args$distanceCutoff
+    
+    #Print if distance cutoff was supplied. Only use cutoff for cis-matrices
+    if(! is.na(distanceCutoff)){
+      print(paste('Removing contacts exceeding', distanceCutoff, 'basepairs'))
+    }
+    
+    
+    #loop through all specified files
+    for (filename in filenames) {
+      print(paste('read in file:',filename))
+      #have to do it the unpractical way as often the matrices contain multiple NAs and the colClasses are estimated as 'character' otherwise
+      colnames_wide <- colnames(fread(file = filename, nrows = 1))
+      mat_wide <- fread(file = filename, colClasses = list(double=2:length(colnames_wide)))
+      
+      #catch error: input matrix not 'square'
+      if(dim(mat_wide)[1]+1 != dim(mat_wide)[2]){
+        stop('Input matrix does not meet shape requirements. Please input a matrix that has coordinates (chr:start-end) as first column so that it has 1 more column than rows ')
+      }
+      
+      #reshape matrix and split location identifiers in 3 separate columns
+      mat_long <-melt(mat_wide, id.vars = colnames_wide[1], measure = colnames_wide[-1])
+      mat_long[, c('A_chrom', 'A_start','A_end') := tstrsplit(get(colnames_wide[1]), ':|-', type.convert=TRUE)]
+      mat_long[, c('B_chrom', 'B_start','B_end') := tstrsplit(variable, ':|-', type.convert=TRUE)]
+      mat_long[, distance := abs(A_start-B_start)]
+  
+      #catch error: trans chromosomal matrix
+      if(length(unique(mat_long$A_chrom)) != 1 |
+         length(unique(mat_long$B_chrom)) != 1){
+        stop('matrix has trans chromosomal interactions. Reshaping to long format with 3 column output (start, stop, value) would loose information')
+      }
+      
+      #Determine which chromosome
+      chromosome_ID_A <- unique(mat_long$A_chrom)
+      chromosome_ID_B <- unique(mat_long$B_chrom)
+      #catch error: more than 1 chromosome in matrix
+      if(length(chromosome_ID_A) > 1|
+         length(chromosome_ID_B) > 1){
+        stop('matrix column or rownames contain more than one chromosome identifer')
+      }
+      #catch error: no cis interactions
+      if(chromosome_ID_A != chromosome_ID_B){
+        stop('Chromosome identiiers of rows and columns not matching')
+      }
+      
+      #remove bins above distance threshold (if specified with args$distanceCutoff)
+      if(! is.na(distanceCutoff)){
+        mat_long <- mat_long[distance <= distanceCutoff]
+      }
+      
+      
+      #select A_start, B_start, value
+      mat_long_three_column <- mat_long[,c('A_start','B_start', 'value')]
+      #mat_long_three_column <- mat_long_three_column[!is.na(mat_long_three_column$value)]#drop empty values
+      mat_long_three_column <- mat_long_three_column[mat_long_three_column$A_start != mat_long_three_column$B_start] #remove diagonal
+      
+      
+      #generate filename:
+      filename_without_path <- unlist(lapply(str_split(filename, pattern='/'), tail, 1))
+      #filename_without_fileending <- unlist(lapply(str_split(filename_without_path, pattern='\\.'), head, 1))
+      filename_long_format <- paste(filename_without_path, 'long.tsv.gz', sep="_")
+      if(length(filenames)==1){
+        filename_outpath = args$outfile
+      }
+      if(length(filenames)>1){
+        filename_outpath = paste0(args$outfile, filename_long_format)
+      }
+      
+      #write to output directory
+      #catch error: no writing permission in output directory
+      if(file.create(filename_outpath) != TRUE){
+        stop('no writing permission for the output directory')
+      }
+      fwrite(mat_long_three_column, filename_outpath, sep = '\t', na=NA)
+    }
+  }
+  
+  ###Convert trans matrices
+    if(scope == 'trans'){
+      
+      #generate filename:
+      filename_outpath = args$outfile
+      #catch error: no writing permission in output directory
+      if(file.create(filename_outpath) != TRUE){
+        stop('no writing permission for the output directory')
+      }
+      
+      #loop through all specified files
+      for (filename in filenames) {
+      print(paste('read in file:',filename))
+      colnames_wide <- colnames(fread(file = filename, nrows = 1))
+      mat_wide <- fread(file = filename, colClasses = list(double=2:length(colnames_wide)))
+      
+      #catch error: input matrix not 'square'
+      if(dim(mat_wide)[1]+1 != dim(mat_wide)[2]){
+        stop('Input matrix does not meet shape requirements. Please input a matrix that has coordinates (chr:start-end) as first column so that it has 1 more column than rows ')
+      }
+      
+      #reshape matrix and split location identifiers in 3 separate columns
+      mat_long <- melt(mat_wide, id.vars = colnames_wide[1], measure = colnames_wide[-1])
+      mat_long[, c('A_chrom', 'A_start','A_end') := tstrsplit(get(colnames_wide[1]), ':|-', type.convert=TRUE)]
+      mat_long[, c('B_chrom', 'B_start','B_end') := tstrsplit(variable, ':|-', type.convert=TRUE)]
+      mat_long[,'interaction_type' := fcase(A_chrom == B_chrom, 'cis',
+                                            default = 'trans')]
+
+      #select A_chrom, A_start, B_chrom, B_start, interaction_type, value
+      mat_long_three_column <- mat_long[,c('A_chrom', 'A_start', 'B_chrom', 'B_start', 'interaction_type', 'value')]
+      #remove NAs
+      mat_long_three_column <- mat_long_three_column[!is.na(mat_long_three_column$value)]
+      #remove diagonal 
+      mat_long_cis <- mat_long_three_column[mat_long_three_column$A_chrom == mat_long_three_column$B_chrom]
+      mat_long_cis <- mat_long_cis[mat_long_cis$A_start != mat_long_cis$B_start] 
+      #gl
+      mat_long_three_column <- rbindlist(list(mat_long_cis, 
+                                         mat_long_three_column[mat_long_three_column$A_chrom != mat_long_three_column$B_chrom]))
+      #sort table
+      mat_long_three_column <- mat_long_three_column[order(A_chrom, A_start, B_chrom, B_start)]
+      
+      #write to output directory
+      fwrite(mat_long_three_column, filename_outpath, sep = '\t', na=NA)
+    }
+  }
+}
+
+main()
